dabc590915f6318a8c77b7a8a913aac3669713bb
Ron Levin <155905928+ronlevin1@users.noreply.github.com>
2025-01-07 19:28:36 +0200

    Add files via upload

---
+++ pepse/PepseGameManager.java
@@ -0,0 +1,9 @@
+package pepse;
+import danogl.*;
+
+public class PepseGameManager extends GameManager {
+
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+}
