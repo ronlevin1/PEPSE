dc4ff3f81a64790619e1f8ca6c94b72db4eb6b7a
ronlevin1 <ron.levin1@mail.huji.ac.il>
2025-01-15 18:54:04 +0200

    cloud rain is OK

--- pepse/world/Cloud.java
+++ pepse/world/Cloud.java
@@ -5,6 +5,7 @@ import danogl.*;
 import danogl.collisions.Layer;
 import danogl.components.CoordinateSpace;
 import danogl.components.Transition;
+import danogl.gui.rendering.OvalRenderable;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
@@ -19,7 +20,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Cloud extends GameObject implements AvatarListener {
-    private static final float RAINDROP_DURATION = 3;
+    private static final float RAINDROP_DURATION = 5;
     private static final Color BASE_CLOUD_COLOR = new Color(255, 255, 255);
     private static final List<GameObject> cloudObjects = new ArrayList<>();
     Consumer<GameObject> addGameObjectCallback;
@@ -110,35 +111,25 @@ public class Cloud extends GameObject implements AvatarListener {
 
     @Override
     public void onAvatarJump() {
-        //todo: make rain drop from cloud
-        System.out.println("Cloud: onAvatarJump -> ITS RAINING");
-        // create a rain drop as a small blue rectangle
-        // set area of possible rain drop spawn as the square of this
-        // cloud object
-        Vector2 TLC = this.getTopLeftCorner();
-        Vector2 RBC = this.getTopLeftCorner().add(this.getDimensions());
-        float lx = TLC.x();
-        float rx = RBC.x();
-//        float ty = TLC.y();
-        float by = RBC.y();
-        int numRainDrops = (int) (Math.random() * 8) + 2;
-        for (int i = 0; i < numRainDrops; i++) {
-            createSingleRaindrop(lx, rx, by);
+        for (GameObject cloudBlock : cloudObjects) {
+            if (Math.random() < 0.3) {
+                createSingleRaindrop(cloudBlock.getTopLeftCorner());
+                createSingleRaindrop(cloudBlock.getTopLeftCorner());
+            }
         }
     }
 
-    private void createSingleRaindrop(float lx, float rx, float by) {
-        // choose a random coordinate in the range of the (lx, rx) and by
-        float randomX = lx + (float) Math.random() * (rx - lx);
-        Vector2 rainDropPos = new Vector2(randomX, by);
+    private void createSingleRaindrop(Vector2 topLeftCorner) {
+        // values
         float size = (float) Block.SIZE / Constants.N_3;
         Vector2 dims = new Vector2(size, size);
         Color rainDropColor = ColorSupplier.approximateMonoColor(
-                new Color(130, 166, 177));
-        Renderable renderable = new RectangleRenderable(rainDropColor);
-        GameObject raindrop = new GameObject(rainDropPos, dims, renderable);
+                new Color(141, 229, 248, 100));
+        Renderable renderable = new OvalRenderable(rainDropColor);
+        // object
+        GameObject raindrop = new GameObject(topLeftCorner, dims, renderable);
         raindrop.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
-        raindrop.transform().setAccelerationY(Constants.GRAVITY);
+        raindrop.transform().setAccelerationY(Constants.GRAVITY / Constants.N_4);
         new Transition<>(
                 raindrop, // the game object being changed
                 raindrop.renderer()::fadeOut, // the method to call
@@ -150,7 +141,5 @@ public class Cloud extends GameObject implements AvatarListener {
                 () -> this.removeGameObjectCallback.accept(raindrop)
         );// nothing further to execute upon reaching final value
         this.addGameObjectCallback.accept(raindrop);
-
-        System.out.println(raindrop.getTopLeftCorner());
     }
 }
