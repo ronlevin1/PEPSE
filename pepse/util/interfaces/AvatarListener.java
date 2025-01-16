5ce37661c7c31c70d09673d6a387c1a9ab715873
Ron Levin <155905928+ronlevin1@users.noreply.github.com>
2025-01-16 19:08:35 +0200

    doc

--- pepse/util/interfaces/AvatarListener.java
+++ pepse/util/interfaces/AvatarListener.java
@@ -4,5 +4,8 @@ package pepse.util.interfaces;
  * Interface for listening to the avatar.
  */
 public interface AvatarListener {
+    /**
+    * execute action when avatar jumps.
+    */
     void onAvatarJump();
 }
